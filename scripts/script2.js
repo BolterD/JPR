4439258a8a0693f7e10d22c5e0674de4149eac54
BolterD <44446416+BolterD@users.noreply.github.com>
2020-05-11 14:58:27 +0100

    Add files via upload

---
+++ scripts/script2.js
@@ -0,0 +1,28 @@
+﻿function finalScore(round) {
+
+    var correct = 0;
+
+    var selectValue;
+
+    var questions = document.getElementsByClassName("question");
+
+    var numOfQuestions = questions.length;
+
+    for (var i = 0; i < questions.length; i++) {
+
+        selectValue = questions[i].options[questions[i].selectedIndex].value;
+
+        if (selectValue === "right"){
+
+            correct++;
+        }
+    }
+
+    if (round === false){
+        document.getElementById("scoreDisplay").innerHTML = (100 / numbOfQuestions) * correct;
+    }
+
+    else {
+        document.getElementById("scoreDisplay").innerHTML = Math.round((100 / numOfQuestions) * correct);
+    }
+}
